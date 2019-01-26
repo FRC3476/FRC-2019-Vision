4c18860cf98785ae6621133e8ae26af2c6f203f9
Ashwin Gupta <ashwingupta2000@gmail.com>
2019-01-26 13:54:13 -0800

    added UDP sender

--- network.cpp
+++ network.cpp
@@ -2,18 +2,23 @@
 #include "network.h"
 #include "arpa/inet.h"
 #include "sys/socket.h"
+#include <cstring>
 
 #define PORT 8000
+#define IP "10.34.76.75"
 
 int sendUDP() {
 	//printf("test \n");
-	struct si;
-	char buffer[256];
+	sockaddr_in server;
+	//char buffer[256];
+	const char *buffer = "test UDP packet";
 	int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); 
-	memset((char *), &si, sizeof(si));	
-	si.sin_family = AF_INET;
-	si.sin_port = htons(PORT);
-	si.sin	
-
+	//bzero(&server, sizeof(server));	
+	memset(&server, 0, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = inet_addr(IP);
+	server.sin_port = htons(PORT);
+	sendto(s, buffer, strlen(buffer)+1, 0, (sockaddr*)&server, sizeof(server));
+	
 	return 0; 
 }
