01d4626cd321c36e04366b4b751dfff255b24b2a
Ashwin Gupta <ashwingupta2000@gmail.com>
2019-02-24 04:56:24 +0000

    hacky fix makes things work :(

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-sleep 20 && /home/ubuntu/Documents/2019/FRC-2019-Vision/FRC-2019-vision static-tests/static5.png>> /tmp/vision.log
-
+#!/bin/bash
+#sleep 5 
+while true; do
+	/home/ubuntu/Documents/2019/FRC-2019-Vision/FRC-2019-vision static-tests/static5.png>> /tmp/vision.log
+done
